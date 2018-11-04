9d8f77d78d0d5026f3222bdf113b459ba3709fe1
chenwu <chenwu>
2018-11-05 00:32:02 +0800

    加油哦！

--- 图书管理系统/js/index.js
+++ 图书管理系统/js/index.js
@@ -57,3 +57,16 @@ function changeImg(){
     lis[index+6].className = "changeColor";
 }
 slideImg();
+
+function setContentTab(name, curr, n) {
+    for (i = 1; i <= n; i++) {
+        var menu = document.getElementById(name + i);
+        var cont = document.getElementById("con_" + name + "_" + i);
+        menu.className = i == curr ? "up" : "";
+        if (i == curr) {
+            cont.style.display = "block";
+        } else {
+            cont.style.display = "none";
+        }
+    }
+}
---
+++ 图书管理系统/js/personal.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Administrator on 2018/11/4 0004.
+ */
+function changetext0()
+{
+    var a =document.getElementsByClassName("both_class");
+    for( var i=0;i<a.length;i++){
+        a[i].style.display='none'
+    }
+    a[0].style.display='block';
+}
+function changetext1()
+{
+    var a =document.getElementsByClassName("both_class");
+    for( var i=0;i<a.length;i++){
+        a[i].style.display='none'
+    }
+    a[1].style.display='block';
+
+}
+
+function changetext2()
+{
+    var a =document.getElementsByClassName("both_class");
+    for( var i=0;i<a.length;i++){
+        a[i].style.display='none'
+    }
+    a[2].style.display='block';
+}
+function changetext3()
+{
+    var a =document.getElementsByClassName("both_class");
+    for( var i=0;i<a.length;i++){
+        a[i].style.display='none'
+    }
+    a[3].style.display='block';
+}
+function changetext4()
+{
+    var a =document.getElementsByClassName("both_class");
+    for( var i=0;i<a.length;i++){
+        a[i].style.display='none'
+    }
+    a[4].style.display='block';
+}
+function changetext5()
+{
+    var a =document.getElementsByClassName("both_class");
+    for( var i=0;i<a.length;i++){
+        a[i].style.display='none'
+    }
+    a[5].style.display='block';
+}
+function changetext6()
+{
+    var a =document.getElementsByClassName("both_class");
+    for( var i=0;i<a.length;i++){
+        a[i].style.display='none'
+    }
+    a[6].style.display='block';
+}
+function changetext7()
+{
+    var a =document.getElementsByClassName("both_class");
+    for( var i=0;i<a.length;i++){
+        a[i].style.display='none'
+    }
+    a[7].style.display='block';
+}
